d4c711745dc77e7b2066939b0925047ab124daad
Robert Valenzuela <robert_valenzuela@usmp.pe>
2021-09-14 18:41:56 -0500

    resuelto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Prog1TeoriaPC2.Models;
+
+namespace Prog1TeoriaPC2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Adopcion()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Adopcion(Mascota objMas)
+        {
+            
+            return RedirectToAction("AdopcionConfirmacion");
+        }
+
+        public IActionResult AdopcionConfirmacion()
+        {
+
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Mascota.cs
@@ -0,0 +1,15 @@
+namespace Prog1TeoriaPC2.Models
+{
+    public class Mascota
+    {
+       public string nombre {get; set;}
+       public string apePat {get;set;}
+       public string apeMat {get;set;}
+       public string tipoMas {get;set;}
+        public string Descripcion {get;set;}
+        public string Telefono {get;set;}
+        public string Distrito {get;set;}
+        public string Direccion {get;set;}
+
+    }
+}
